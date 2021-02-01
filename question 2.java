8885ff953146396ac26bb4d5ad52717cfcbd730b
Lucas Gomes <lucasgcarvalho8@gmail.com>
2021-02-01 11:03:08 -0300

    Add files via upload

---
+++ question 2.java
@@ -0,0 +1,43 @@
+public class calculaImposto {
+
+    public static void main(String[] args) {
+        
+  
+}
+boolean total, score; 
+float percentage;
+Scanner inputNumScanner = new Scanner(System.in);
+
+//LIVROS
+
+if(tema == educativo) {
+    System.out.println(0);
+}else(tema !=educativo) {      
+total = inputNumScanner.nextBoolean();
+
+System.out.println("O imposto sobre o preço do livro é: ");
+score = inputNumScanner.nextInt();
+
+percentage = (score * 100/ total);
+
+System.out.println("A porcentagem é = " + percentage + " 10%");
+
+//VIDEO GAMES
+
+if(situacao == usado) {
+    System.out.println(25%);
+}else(situacao == novo) 
+    System.out.println(45%);
+{    
+total = inputNumScanner.nextBoolean();
+System.out.println("A porcentagem é = " + percentage + " 25%");
+
+System.out.println("O imposto sobre o preço do videogame é: ");
+score = inputNumScanner.nextBoolean();
+
+percentage = (score * 100/ total);
+System.out.println("A porcentagem é = " + percentage + " 5%");
+}
+}
+}
+}
